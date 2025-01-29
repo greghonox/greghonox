c2176cd8de70ed64deea9bff40d1b72892deac77
Gregorio honorato <greghono@gmail.com>
2025-01-29 20:16:34 -0300

    fix: improve the message

--- pages/call_me.py
+++ pages/call_me.py
@@ -12,4 +12,6 @@ msg_default = "Não esqueça de deixar seu contato para que eu possa responder..
 prompt = st.chat_input("Escreva algo que receberei no telegram..." + msg_default)
 if prompt:
     TeleGram(prompt)
-    st.success("Mensagem enviada com sucesso!")
+    st.success(
+        "Mensagem enviada mim, de preferencia deixe seu contato para mim responder!"
+    )
